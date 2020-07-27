bdc376275472fed64801c5c10b9bb303ee949821
stali1234 <stalin.sangeethraj@gmail.com>
2020-07-27 09:25:25 -0400

    day2

---
+++ linear.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+ 
+
+int main(){
+    vector<int> v;
+    int n,x;
+    int key;
+    int count;
+    cout<<"enter the size of an array"<<endl;
+    cin>>n;
+    cout<<"enter the array elements "<<endl;
+    for(int i=0;i<n;i++)
+    {
+
+        cin>>x;
+        v.push_back(x);
+    }
+    cout<<"enter the key element to be searched"<<endl;
+    cin>>key;
+
+    count = 0;
+    while (count <= v.size())
+    {
+        if(v[count]==key)
+        {
+            cout<<"element found at the location"<<count+1;
+        }
+         
+        count++;
+        
+    }
+    return 0;
+    
+}
